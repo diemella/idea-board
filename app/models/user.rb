56dbb25da70628f6ed3a5901c899fb1b22913e37
Diem Hoang <hoangd5@gmail.com>
2017-10-27 15:30:38 -0500

    generate model and migration for user

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # validations
+  validates :email, :username, presence: true, uniqueness: true
+
+  # associations
+  has_many :ideas, foreign_key: "creator_id"
+
+  def password
+    @password ||= BCrypt::Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.encrypted_password = @password
+  end
+
+  def authenticate(inputted_password)
+    self.password == inputted_password
+  end
+end

c73f871fa4526786c44e12c7a9f6d80f4e7a28a5
Diem Hoang <hoangd5@gmail.com>
2017-10-27 15:37:37 -0500

    add routes and controller, helper methods, and views for user

---
+++ app/controllers/user.rb
@@ -0,0 +1,53 @@
+# user controller
+
+# register
+get '/users/register' do
+  erb :'users/register'
+end
+
+post '/users' do
+  @user = User.new(params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @error = "Invalid input!"
+    erb :'/users/register'
+  end
+end
+
+
+# login
+
+get '/users/login' do
+  erb :'/users/login'
+end
+
+post '/users/login' do
+  @user = User.find_by(email: params[:email])
+
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @error = "Invalid input!"
+    erb :'/users/login'
+  end
+end
+
+# logout
+get '/users/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
+
+# profile
+
+get '/users/profile' do
+  if current_user
+    @items = current_user.items
+    erb :'/users/profile'
+  else
+    redirect '/users/login'
+  end
+end
